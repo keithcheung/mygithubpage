f765271d22faa4cd4ad8a57583c9948f75d0d23b
Keith <keith.cheung41@gmail.com>
2019-07-21 22:44:48 -0400

    upgrade lodash

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0307ac5e1aba9a887f17a40354e61920.js"
+  "/precache-manifest.518ccd300e54a844577680a5ca9f87f5.js"
 );
 
 self.addEventListener('message', (event) => {

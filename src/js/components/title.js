9ce8076515120acd2d10838b8d937c4b7d50589e
Keith <keith.cheung41@gmail.com>
2019-11-29 01:56:54 -0500

    more updates

--- src/js/components/title.js
+++ src/js/components/title.js
@@ -22,8 +22,8 @@ function Title() {
     <div>
       <H3>Keith Cheung</H3>
       <p>
-        CS Student at the University of Waterloo | Incoming UI Engineer Intern
-        at LinkedIn | Previously at HubSpot (x2) and SMART Technologies
+        CS Student at the University of Waterloo / Incoming UI Engineer Intern
+        at LinkedIn Previously at HubSpot (x2) and SMART Technologies
       </p>
       <hr />
       <div>
